7a4e384e03cd86a9762275e272f1ae040201d8dd
Samantha Bennett <samanthabennett@ufl.edu>
2025-10-29 21:35:54 -0400

    implement dp bottom-up method

--- program4B.py
+++ program4B.py
@@ -14,11 +14,32 @@ def program4B(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
     int:  maximal total value
     List[int]: the indices of the chosen vaults(1-indexed)
     """
-    ############################
-    # Add you code here
-    ############################
+    # initialize the OPT array and first element 
+    OPT = [0] * (n+1)
+    OPT[0] = 0
+    vaults = []
 
-    return 0, [1, 2, 3] # replace with your code
+    # fill the OPT array
+    for i in range(1, n + 1):
+        include_value = values[i - 1]
+        if i - k - 1 > 0:
+            include_value += OPT[i - k - 1]
+        last_value = OPT[i - 1]
+        if include_value > last_value:
+            OPT[i] = include_value
+        else:
+            OPT[i] = last_value
+
+    # backtrack to find which vaults were chosen
+    i = n
+    while i > 0:
+        if OPT[i] != OPT[i - 1]:
+            vaults.append(i)
+            i -= k + 1
+        else:
+            i -= 1
+    
+    return OPT[n], vaults[::-1]
 
 
 if __name__ == '__main__':
