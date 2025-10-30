6c03e0aa15a9322f8fcd61bd314773cc25c22ef5
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2025-10-29 20:52:18 -0400

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,32 @@
+from typing import List, Tuple
+
+
+def program5(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
+    """
+    Solution to Program 5
+    
+    Parameters:
+    n (int): number of vaults
+    k (int): no two chosen vaults are within k positions of each other
+    values (List[int]): the values of the vaults
+
+    Returns:
+    int:  maximal total value
+    List[int]: the indices of the chosen vaults(1-indexed)
+    """
+    ############################
+    # Add you code here
+    ############################
+
+    return 0, [1, 2, 3] # replace with your code
+
+
+if __name__ == '__main__':
+    n, k = map(int, input().split())
+    values = list(map(int, input().split()))
+
+    m, indices = program5(n, k, values)
+
+    print(m)
+    for i in indices:
+        print(i)
