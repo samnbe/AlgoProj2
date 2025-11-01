41c28e3db430d32741dcfb05082e1d71813a6385
Samantha Bennett <samanthabennett@ufl.edu>
2025-11-01 15:08:44 -0400

    implemented algorithm for bottom-up O(n^2)

--- program4B.py
+++ program4B.py
@@ -14,7 +14,28 @@ def program4B(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
     int:  maximal total value
     List[int]: the indices of the chosen vaults(1-indexed)
     """
-    return 0, []  # Placeholder return statement
+    OPT = [0] * (n+1)
+    OPT[0] = 0
+    vaults = []
+
+    for i in range(1, n + 1):
+        OPT[i] = OPT[i - 1]
+        for j in range(1, i+1):
+            include_value = values[j - 1]
+            if j - k - 1 > 0:
+                include_value += OPT[j - k - 1]
+            OPT[i] = max(OPT[i], include_value)
+    
+    # backtrack to find which vaults were chosen
+    i = n
+    while i > 0:
+        if OPT[i] != OPT[i - 1]:
+            vaults.append(i)
+            i -= k + 1
+        else:
+            i -= 1
+
+    return OPT[n], vaults[::-1]  # Placeholder return statement
 
 
 if __name__ == '__main__':
