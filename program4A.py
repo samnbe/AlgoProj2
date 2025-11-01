272a7e84e031852b1a2af6c6992304036b2c5caa
Samantha Bennett <samanthabennett@ufl.edu>
2025-11-01 17:54:32 -0400

    implemented top-down recursion algo

--- program4A.py
+++ program4A.py
@@ -14,10 +14,41 @@ def program4A(n: int, k: int, values: List[int]) -> Tuple[int, List[int]]:
     int:  maximal total value
     List[int]: the indices of the chosen vaults(1-indexed)
     """
+    OPT = [-1] * (n + 1)
+    vaults = []
    
 
-    return 0, []  # Placeholder return statement
+    def compute_OPT(i: int) -> int:
+        if i <= 0:
+            return 0
+        if OPT[i] != -1:
+            return OPT[i]
+        
+        exclude_value = compute_OPT(i - 1)
+        
+        for j in range(1, i+1):
+            include_value = values[j - 1]
+            if j - k - 1 > 0:
+                include_value += compute_OPT(j - k - 1)
+            OPT[i] = max(exclude_value, include_value)
 
+        # include_value = values[i - 1] + compute_OPT(i - k - 1)
+        # exclude_value = compute_OPT(i - 1)
+
+        # OPT[i] = max(include_value, exclude_value)
+        return OPT[i]
+    
+    ans = compute_OPT(n)
+
+    i = n
+    while i > 0:
+        if OPT[i] != OPT[i - 1]:
+            vaults.append(i)
+            i -= k + 1
+        else:
+            i -= 1
+
+    return ans, vaults[::-1]
 
 if __name__ == '__main__':
     n, k = map(int, input().split())
